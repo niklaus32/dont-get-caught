23f772c55d726c9f6af0aa61ad5d98e75dcbfcd7
niklaus32 <niklaus6345@gmail.com>
2025-08-03 03:51:49 +1200

    added text esc to exit

--- src/pencilbox.js
+++ src/pencilbox.js
@@ -14,14 +14,12 @@ export function createPencilBox() {
     ]);
     pencilBox.onHover(() => {
     if (!pencilBox.isEnlarged) {
-        debug.log("pencilbox hovered");
         pencilBox.scale = vec2(pencilBoxSize + 0.3);        
         pencilBox.color = rgb(255, 220, 220); 
     }
     });
     pencilBox.onHoverEnd(() => {
     if (!pencilBox.isEnlarged) {
-        debug.log("pencilbox unhovered");
         pencilBox.scale = vec2(pencilBoxSize);          
         pencilBox.color = rgb(255, 255, 255);
         pencilBox.showWriting = false;
@@ -66,6 +64,23 @@ export function createPencilBox() {
     }
     });
     const secretNotes = createNotes(pencilBox);
+
+
+    loadBitmapFont("unscii", "public/examples/fonts/unscii_8x8.png", 8, 8);
+    const exitText = add([
+        text("Press ESC to exit", {
+            font: "unscii",
+            size: 15,
+        }),
+        pos(20, 20),
+        color(rgb(255, 255, 255)),
+        layer(),
+        "exitText",
+    ]);
+    exitText.opacity = 0;
+    onUpdate("openedPencilBox", () => {
+        exitText.opacity = pencilBox.isEnlarged ? 1 : 0;
+    });
   return pencilBox;
 }
 
