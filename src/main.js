0a1c51faffd13f3a4d0438b503517e4c67049d83
niklaus32 <niklaus6345@gmail.com>
2025-08-02 18:39:02 +1200

    created pencilbox

--- src/main.js
+++ src/main.js
@@ -71,3 +71,27 @@ const showPhone = () => {
 
   crew.z = 1;
 };
+
+
+// Load pencilbox sprite
+loadSprite("pencilBox", "sprites/bean.png");
+
+// Add pencilbox with interactivity
+const pencilBox = add([
+  sprite("pencilBox"),
+  pos(250, 400),
+  scale(1),
+  area(), // enables hover/click
+  "pencilBox",
+]);
+
+pencilBox.onHover(() => {
+    debug.log("pencilbox hovered");
+    pencilBox.scale = vec2(1.1);        // Scale up
+    pencilBox.color = rgb(255, 220, 220); // Slightly tinted
+});
+pencilBox.onHoverEnd(() => {
+    debug.log("pencilbox unhovered");
+    pencilBox.scale = vec2(1);          // Reset scale
+    pencilBox.color = rgb(255, 255, 255);
+});
