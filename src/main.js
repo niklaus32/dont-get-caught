0037a28393feea85089a2c80f057ee55254b2293
niklaus32 <niklaus6345@gmail.com>
2025-08-02 19:30:29 +1200

    smooth enlarge animation

--- src/main.js
+++ src/main.js
@@ -80,12 +80,12 @@ onKeyPress("i", () => {
 
 
 // Load pencilbox sprite
-loadSprite("pencilBox", "sprites/bean.png");
+loadSprite("pencilBox", "./public/examples/sprites/grass.png");
 
 // Add pencilbox with interactivity
 const pencilBox = add([
   sprite("pencilBox"),
-  pos(250, 400),
+  pos(width()/2, height() / 2),
   scale(1),
   area(), // enables hover/click
   "pencilBox",
@@ -93,11 +93,73 @@ const pencilBox = add([
 
 pencilBox.onHover(() => {
     debug.log("pencilbox hovered");
-    pencilBox.scale = vec2(1.1);        // Scale up
+    pencilBox.scale = vec2(1.5);        // Scale up
     pencilBox.color = rgb(255, 220, 220); // Slightly tinted
 });
 pencilBox.onHoverEnd(() => {
     debug.log("pencilbox unhovered");
     pencilBox.scale = vec2(1);          // Reset scale
     pencilBox.color = rgb(255, 255, 255);
+    pencilBox.showWriting = false;
+    pencilBox.isEnlarged = false;
+});
+
+// Enlarge and show writing on click
+pencilBox.onClick(() => {
+  if (!pencilBox.isEnlarged) {
+    // Animate scale up with a tween
+    tween(
+      pencilBox.scale,
+      vec2(5),
+      0.5, // duration in seconds
+      (val) => {
+        pencilBox.scale = val;
+      },
+      easings.easeOutBack // smooth, bouncy effect
+    ).then(() => {
+      pencilBox.isEnlarged = true;
+      pencilBox.showWriting = true;
+    });
+  } else {
+    // Animate scale down
+    tween(
+      pencilBox.scale,
+      vec2(1),
+      0.3,
+      (val) => {
+        pencilBox.scale = val;
+      },
+      easings.easeInOutSine
+    ).then(() => {
+      pencilBox.isEnlarged = false;
+      pencilBox.showWriting = false;
+    });
+  }
+});
+
+// Display writing inside when enlarged
+let pencilboxWriting = null;
+pencilBox.onUpdate(() => {
+  if (pencilBox.showWriting) {
+    if (!pencilboxWriting) {
+      pencilboxWriting = add([
+        text("My secret notes!", { size: 32 }),
+        pos(pencilBox.pos.x + pencilBox.width / 4, pencilBox.pos.y + pencilBox.height / 2),
+        color(BLACK),
+        z(10),
+        "pencilboxWriting",
+      ]);
+    } else {
+      // Update position if pencilBox moves or scales
+      pencilboxWriting.pos = vec2(
+        pencilBox.pos.x + pencilBox.width * pencilBox.scale.x / 4,
+        pencilBox.pos.y + pencilBox.height * pencilBox.scale.y / 2
+      );
+    }
+  } else {
+    if (pencilboxWriting) {
+      destroy(pencilboxWriting);
+      pencilboxWriting = null;
+    }
+  }
 });
